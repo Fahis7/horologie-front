33879ee91e6e73cc69fafa53c1383bdc33ed660c
Fahis7 <fahismt777@gmail.com>
2026-01-16 17:27:30 +0530

    .env

--- api/Api.js
+++ api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
